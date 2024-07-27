fc48c6321f07bb1de3b92cabb3755affe3328af2
unknown <lokeswararaodumpala2004@gmail.com>
2024-07-28 00:53:58 +0530

    Initial commit without .env file

---
+++ language.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int m;
+    cout<<"enter hoew much money do you have"<<endl;
+    cin>>m;
+    if(m < 100){
+        cout<<"great you hav lot of money"<<endl;
+
+    }
+    else{
+        cout<<"no problem"<<endl;
+    }
+    return 0;
+}
