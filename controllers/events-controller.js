d9bb966980bc7bf9e9dceca36327ef8aac33e34c
LOKESH DUMPALA <133016649+LOKESHDUMPALA@users.noreply.github.com>
2025-02-19 12:58:09 +0530

    Update events-controller.js

--- controllers/events-controller.js
+++ controllers/events-controller.js
@@ -1,6 +1,32 @@
 const PostEvents = require("../models/PostEvent");
 const Students = require("../models/Student");
-const { sendEmail } = require("../config/email");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendEmail = async (to, subject, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    });
+    console.log("Email sent:", info.messageId);
+    return true;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return false;
+  }
+};
 
 exports.postEvent = async (req, res) => {
     try {
